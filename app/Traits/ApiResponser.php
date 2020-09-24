200f7645c127591dd05be5ad8ffabc04e7f0e46b
GKentaurus <gkent@outlook.com>
2020-09-23 22:09:46 -0500

    ApiResponser adaptado para nuevas respuestas

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -7,6 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 trait ApiResponser
 {
+  protected function showAll(Collection $collection, $code = 200)
+  {
+    return $this->successResponse(['data' => $collection], $code);
+  }
+
+  protected function showOne(Model $instance, $code = 200)
+  {
+    return $this->successResponse(['data' => $instance], $code);
+  }
+
+  protected function successResponse($data, $code)
+  {
+    return response()->json($data, $code);
+  }
+
   protected function errorResponse($message, $code)
   {
     return response()->json(
@@ -16,20 +31,5 @@ trait ApiResponser
       ],
       $code
     );
-  }
-
-  protected function showAll(Collection $collection, $code = 200)
-  {
-    return $this->successResponse(['data' => $collection], $code);
-  }
-
-  private function successResponse($data, $code)
-  {
-    return response()->json($data, $code);
-  }
-
-  protected function showOne(Model $instance, $code = 200)
-  {
-    return $this->successResponse(['data' => $instance], $code);
   }
 }
