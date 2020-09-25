32168579b1c6f06d312ed3ee7f50dec818e48b93
GKentaurus <gkent@outlook.com>
2020-09-25 15:34:35 -0500

    Variables globales de la aplicación para seeders

--- config/app.php
+++ config/app.php
@@ -243,4 +243,22 @@ return [
   'dropCarts' => false,
   'dropCartContents' => false,
 
+  /**
+   * ANCHOR Constantes para los Database Seeders
+   * $numberFactories => integer
+   */
+  'seedAddressTable' => 8, // NOTE  min seedCompanyTable * 2
+  'seedCartTable' => 4, //  NOTE min = seedUserTable
+  'seedCardContentTable' => 50,
+  'seedCompanyTable' => 4,  //  NOTE min = seedUserTable
+  'seedDocumentTypeTable' => 4, // NOTE min = 4
+  'seedOrderTable'  => 4, // NOTE min <= seedCartTable
+  'seedOrderStatusTable' => 10,  // NOTE min = 7
+  'seedPaymentMethodTable' => 10, // NOTE min = 4
+  'seedPriceListTable' => 4, // NOTE min = 4
+  'seedPriceTable' => 5, // NOTE min = 5
+  'seedProductTable' => 5, // NOTE min = 5
+  'seedProductCategoryTable' => 10, // NOTE min = 1
+  'seedUserCategoryTable' => 4, // NOTE min = 4
+  'seedUserTable' => 4, // NOTE min = 4
 ];
