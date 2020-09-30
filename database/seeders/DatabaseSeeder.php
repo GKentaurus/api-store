34871762e1802b178634c130c47ebecaa7d29705
GKentaurus <gkent@outlook.com>
2020-09-29 23:12:15 -0500

    Restauración de Seeders para OrderStatus y PaymentMethod

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -43,8 +43,8 @@ class DatabaseSeeder extends Seeder
       // CartSeeder::class,
       // CartContentSeeder::class,
       ProductCategorySeeder::class,
-      // OrderStatusSeeder::class,
-      // PaymentMethodSeeder::class,
+      OrderStatusSeeder::class,
+      PaymentMethodSeeder::class,
       // OrderSeeder::class,
     ]);
   }
